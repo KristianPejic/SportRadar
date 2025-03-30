1808ed7ca67ad4b413a6bc56499f0f03987513b3
Kristian Pejić <108726003+KristianPejic@users.noreply.github.com>
2025-03-31 01:25:21 +0200

    Implementing match model for teams and scores

---
+++ src/main/java/com/sportradar/worldcup/model/Match.java
@@ -0,0 +1,72 @@
+package com.sportradar.worldcup.model;
+
+import java.time.LocalDateTime;
+
+public class Match {
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+    private LocalDateTime startTime;
+
+    public Match() {
+    }
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = 0;
+        this.awayScore = 0;
+        this.startTime = LocalDateTime.now();
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public int getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(int homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public int getAwayScore() {
+        return awayScore;
+    }
+
+    public void setAwayScore(int awayScore) {
+        this.awayScore = awayScore;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getTotalScore() {
+        return homeScore + awayScore;
+    }
+
+    @Override
+    public String toString() {
+        return homeTeam + " " + homeScore + " - " + awayTeam + " " + awayScore;
+    }
+
+}
